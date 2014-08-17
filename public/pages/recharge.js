99aa327ef3a4aaf9f1163e52d2a1ec0406bcd334
supersheep <supersheepxjy@gmail.com>
2014-08-17 09:03:18 +0800

    充值请求

--- public/pages/recharge.js
+++ public/pages/recharge.js
@@ -8,3 +8,19 @@ $(".choices .row").on("touchend",function(){
   el.addClass("active");
   current = el;
 });
+
+$(".button").on("touchend",function(){
+
+  var price = $(".row.active").attr("data-price");
+
+  if(!price){
+    alert("请选择充值金额");
+    return;
+  }
+
+  $.post("/api/v1/recharge/" + price).done(function(){
+    location.href = "/";
+  }).fail(function(){
+  });
+
+});
