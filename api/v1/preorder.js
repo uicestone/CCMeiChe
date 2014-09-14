8058f611aa4e26754265550f9e17f9d95b2695e2
supersheep <supersheepxjy@gmail.com>
2014-09-14 10:26:25 +0800

    filter worker by last interaction time

--- api/v1/preorder.js
+++ api/v1/preorder.js
@@ -73,6 +73,9 @@ function findWorkers(latlng,callback){
     openid:{
       $ne: null
     },
+    last_intraction_time:{
+      $gt: new Date(+new Date() - 24 * 60 * 60 * 1000)
+    },
     status:"on_duty",
     latlng:{
       $near: latlng
