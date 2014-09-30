3d1a8f1b5aa123548787d77268dff28535311560
supersheep <supersheepxjy@gmail.com>
2014-09-30 20:36:41 +0800

    add services

--- routes/services.js
+++ routes/services.js
@@ -1,5 +1,13 @@
+var Service = require('../model/service');
+
 module.exports = function(req,res){
-  res.render('services',{
-    user: req.user
+  Service.find().sort({
+    _id: 1
+  }).toArray(function(err, services){
+    res.render('services',{
+      id:"recharge",
+      user: req.user,
+      services: services
+    });
   });
 }
