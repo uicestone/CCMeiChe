36f8564f1283521e45204c150ff6ecc0dc2060ff
supersheep <supersheepxjy@gmail.com>
2014-11-03 16:15:30 +0800

    use the real ip

--- util/wechat.js
+++ util/wechat.js
@@ -56,7 +56,7 @@ var pay_request = function(req, order, callback){
     'attach': order_attach,
     'out_trade_no': order_id,
     'total_fee': (total_price * 1),
-    'spbill_create_ip': req.ip,
+    'spbill_create_ip': req.header('x-forwarded-for') || req.connection.remoteAddress,
     "openid": req.user.openid,
     "trade_type": "JSAPI"
   };
