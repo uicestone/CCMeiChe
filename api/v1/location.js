9e94fb15700461fd1a8699078ea0a91c14f5fefc
supersheep <supersheepxjy@gmail.com>
2014-11-09 10:50:13 +0800

    坐标转换纠偏

--- api/v1/location.js
+++ api/v1/location.js
@@ -1,10 +1,19 @@
 var baidumap = require('../../util/baidumap');
-
-exports.latlng = function(req,res){
-  baidumap.geocoder({
-    pois: 1,
-    location: [req.params.lat,req.params.lng].join(",")
-  },function(err, result){
+var async = require('async');
+exports.latlng = function(req,res,next){
+  var loc = [req.params.lng,req.params.lat].join(",");
+  async.waterfall([function(done){
+    baidumap.geoconv({
+      coords: loc,
+      from: 1,
+      to: 5
+    }, done);
+  },function(json, done){
+    baidumap.geocoder({
+      pois: 1,
+      location: json.result[0].y + "," + json.result[0].x
+    },done);
+  }], function(err, result){
     if(err){return next();}
     res.status(200).send(result);
   });
