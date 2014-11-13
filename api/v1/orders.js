6dc75b93677f5960d1af69e9d246ec125ec5849a
supersheep <supersheepxjy@gmail.com>
2014-11-13 13:48:00 +0800

    update message

--- api/v1/orders.js
+++ api/v1/orders.js
@@ -102,7 +102,12 @@ exports.arrive = function(req,res,next){
 
     async.series([
       function(done){
-        var message = "车工已到达并开始为您洗车";
+        var message;
+        if(order.service.needopen){
+          message = "您的CC美车管家已经到达，麻烦您解锁爱车，并取走车上贵重物品。谢谢您的配合。";
+        }else{
+          message = "您的CC没车管家已经到达并已开始作业，请耐心等候。";
+        }
         wechat_user.sendText(order.user.openid, message, done);
       },
       function(done){
