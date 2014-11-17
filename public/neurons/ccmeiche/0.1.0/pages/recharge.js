f6a91b8d5186686f40fe48fc04878da04e58013f
supersheep <supersheepxjy@gmail.com>
2014-11-17 13:39:24 +0800

    update

--- public/neurons/ccmeiche/0.1.0/pages/recharge.js
+++ public/neurons/ccmeiche/0.1.0/pages/recharge.js
@@ -69,7 +69,7 @@ $(".button").on("tap",function(){
         }
         setTimeout(function(){
           location.href = "/wechat/?showwxpaytitle=1";
-        },2000);
+        },5000);
       });
     }else{
       WeixinJSBridge.invoke('getBrandWCPayRequest',payment_args,function(res){
